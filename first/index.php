3b8f41e537faa8429f6919f762adec976cd69dd6
leonardotavares20 <leo.tav.12345@gmail.com>
2025-02-12 16:36:26 -0300

    feat: adiciona primeira tag em php

---
+++ first/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Inicie com php -S localhost:porta -->
+    <h1>
+    <?php
+        echo "Hello world";
+    ?>
+    </h1>
+</body>
+</html>
